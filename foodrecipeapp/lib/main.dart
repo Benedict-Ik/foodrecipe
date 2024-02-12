a1f83ce46d789e317ca7d86d07b3944e1c45e1eb
Benedict Odoh <benedict.ik98@gmail.com>
2024-02-12 12:07:49 +0100

    Added Profile and Card sections

--- foodrecipeapp/lib/main.dart
+++ foodrecipeapp/lib/main.dart
@@ -38,12 +38,14 @@ class MyApp extends StatelessWidget {
           ),
           body: Padding(
             padding: EdgeInsets.only(left: 10.0, right: 10.0),
+
+            // Parent Column
             child: Column(children: [
               // Header text description
               Row(
                 children: [
                   Text(
-                    "How to make french toast",
+                    "How to make french\ntoast ",
                     textDirection: TextDirection.ltr,
                     style: TextStyle(
                         fontFamily: "Poppins",
@@ -53,6 +55,36 @@ class MyApp extends StatelessWidget {
                 ],
               ),
               // Banner Image
+              // Column(
+              //   children: [
+              //     Stack(
+              //       children: [
+              //         ClipRRect(
+              //           borderRadius: BorderRadius.circular(10.0),
+              //           child: Image.asset(
+              //             "ui-assets/img/banner.png",
+              //             width: double.infinity,
+              //             height: 100.0,
+              //             fit: BoxFit.contain,
+              //           ),
+              //         ),
+              //         Positioned(
+              //           top:
+              //               0, // Adjusted to position the image centrally vertically
+              //           left:
+              //               0, // Adjusted to position the image centrally horizontally
+              //           child: Image.asset(
+              //             "ui-assets/img/play-button.png",
+              //             width: 100.0,
+              //             height: 100.0,
+              //             fit: BoxFit.cover,
+              //           ),
+              //         ),
+              //       ],
+              //     ),
+              //   ],
+              // ),
+
               Column(
                 children: [
                   ClipRRect(
@@ -107,19 +139,108 @@ class MyApp extends StatelessWidget {
                     ),
                   ],
                 ),
-                // Row(
-                //   children: [
-                //     Text(
-                //       "4,5",
-                //       textDirection: TextDirection.ltr,
-                //       style: TextStyle(
-                //           fontFamily: "Poppins",
-                //           fontWeight: FontWeight.w700,
-                //           fontSize: 10.0),
-                //     ),
-                //   ],
-                // ),
-              ])
+              ]),
+
+              // Profile section
+
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Image.asset(
+                        "ui-assets/img/profile-img.png",
+                        width: 50.0,
+                        height: 50.0,
+                        // fit: BoxFit.contain,
+                      ),
+                      SizedBox(
+                          width:
+                              10), // Add spacing between the profile image and text
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Roberta Anny",
+                            style: TextStyle(
+                              fontFamily: "Roboto",
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                          Row(
+                            children: [
+                              Image.asset(
+                                "ui-assets/img/location.png",
+                                width: 20.0,
+                                height: 20.0,
+                                // fit: BoxFit.contain,
+                              ),
+                              SizedBox(
+                                  width:
+                                      5), // Add spacing between the location icon and text
+                              Text(
+                                "Bali, Indonesia",
+                                style: TextStyle(
+                                  fontSize: 14.0,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      Spacer(), // Add spacer to push the follow button to the right
+                      ElevatedButton(
+                        onPressed: () {
+                          // Add functionality for the follow button press
+                        },
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.red, // Background color
+                          onPrimary: Colors.white, // Text color
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 10),
+                        ),
+                        child: Text("Follow"),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+
+              // Card section
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                        vertical: 20.0), // Add vertical padding
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Ingredients",
+                          style: TextStyle(
+                            fontFamily: "Poppins",
+                            fontWeight: FontWeight.w700,
+                            fontSize: 18.0,
+                          ),
+                        ),
+                        Text(
+                          "5 items",
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 14.0,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+
+              // Individual Cards
             ]),
           )),
     );
